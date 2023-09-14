39476551ae640e526c8710ca1a451e53b5ce02e8
Ninad Phute <ninad.phute@Ninads-MacBook-Pro.local>
2023-09-14 12:04:17 +0530

    Delete unnecessary listener class

--- src/main/java/org/tw/bootcamp/ParkingNotificationListener.java
+++
@@ -1,9 +0,0 @@
-package org.tw.bootcamp;
-
-public class ParkingNotificationListener implements ParkingLotObserver {
-
-    @Override
-    public void notifyParkingStatus(String message) {
-
-    }
-}
